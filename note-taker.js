f84e1c5c9a4faf5ce0a9263dcc6038bc7bf559f4
Dylan Coffey <dcoffey20@gmail.com>
2020-01-22 18:45:17 -0600

    get and post work.

--- note-taker.js
+++ note-taker.js
@@ -3,13 +3,37 @@ const path = require("path");
 const fs = require("fs");
 
 let app = express();
+let data = JSON.parse(fs.readFileSync("./db/db.json"))
+let id=[];
 
 let PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.get("/",function(req, res){
-    res.sendFile(path.join(__dirname, "index.html"))
+// GET for HTML pages, js, and css files
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "./public/index.html"))
 })
-app.get("/")
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "./public/notes.html"))
+})
+
+// API GET, POST, and Delete
+app.get("/api/notes", function(req, res){
+    res.json(data)
+    console.log(data[0].id)
+})
+app.post("/api/notes", function(req, res){
+    data.push(req.body);
+    fs.writeFile("./db/db.json", JSON.stringify(data), "utf8", (err, data) => {if(err)throw err})
+
+})
+// app.delete("/api/notes", function(req, res){
+//     
+// })
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT" + PORT)
+});
